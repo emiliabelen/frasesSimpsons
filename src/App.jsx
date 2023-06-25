44e53204feb57c1fbee7ac64be0356d992551926
belen ramos <ramos.emiliabelen@gmail.com>
2023-06-25 00:03:35 -0300

    se creo la estructura inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./assets/logosimpson.png";
+import { Button, Container, mostrarSpinner } from "react-bootstrap";
+import Frase from "./components/Frase";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    try {
+      //vamos hacer una peticion GET
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      const datos = await respuesta.json();
+      console.log(respuesta);
+      console.log(datos[0]);
+      setPersonaje(datos[0]);
+    } catch (errores) {
+      console.log(errores);
+    }
+  };
+
+  const mostrarComponente = mostrarSpinner ? (
+    <div className=" my-5 Spinner">
+      <div class="rect1"></div>
+      <div class="rect2"></div>
+      <div class="rect3"></div>
+      <div class="rect4"></div>
+      <div class="rect5"></div>
+    </div>
+  ) : (
+    <Frase personaje={personaje} />
+  );
+
+  return (
+    <>
+      <Container className="text-center my-5">
+        <img src={logo} alt="Logo de los simpson" className="w-50" />
+        {mostrarComponente}
+
+        <Button variant="warning" onClick={consultarAPI}>
+          Obtener frase
+        </Button>
+      </Container>
+    </>
+  );
+}
+
+export default App;
